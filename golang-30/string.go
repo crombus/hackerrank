26e3fbfb522be904a325e617c54d223a866b78eb
crombus <pkundra@redhat.com>
2021-02-25 13:20:13 +0530

    add go for strings question

---
+++ golang-30/string.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	str := "THIS IS A TEST TEXT"
+	fmt.Println(strings.Contains(str, "TeST"))
+	fmt.Println(strings.Count(str, "TEST"))
+	fmt.Println(str[1:])
+
+	str1 := "AABAACAADAABAABA"
+	pat := "AABA"
+
+	h := true
+	for h {
+		in := strings.Index(str1, pat)
+		str1 = str1[in+len(pat):]
+		fmt.Println(in)
+		if !strings.Contains(str1, pat) {
+			h = false
+		}
+	}
+}
